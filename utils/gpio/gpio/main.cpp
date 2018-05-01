a8711783e8fa952de79a07b7835f4514995d69d3
epsilonrt <epsilonrt@gmail.com>
2018-05-01 16:14:08 +0200

    warranty() without file depends

--- utils/gpio/gpio/main.cpp
+++ utils/gpio/gpio/main.cpp
@@ -583,14 +583,21 @@ version() {
 // -----------------------------------------------------------------------------
 void
 warranty (int argc, char * argv[]) {
-  std::ostringstream filepath;
+  cout <<
+       "Copyright © 2018 " << authors << ", All rights reserved.\n\n"
 
-  filepath << PIDUINO_INSTALL_DATA_DIR << "/WARRANTY";
-  ifstream f (filepath.str());
+       " Piduino pinfo is free software: you can redistribute it and/or modify\n"
+       " it under the terms of the GNU General Public License as published by\n"
+       " the Free Software Foundation, either version 3 of the License, or\n"
+       " (at your option) any later version.\n\n"
 
-  if (f.is_open()) {
-    cout << f.rdbuf();
-  }
+       "  Piduino pinfo is distributed in the hope that it will be useful,\n"
+       " but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+       " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+       " GNU General Public License for more details.\n\n"
+
+       " You should have received a copy of the GNU General Public License\n"
+       " along with Piduino pinfo. If not, see <http://www.gnu.org/licenses/>.\n";
 }
 
 // -----------------------------------------------------------------------------
