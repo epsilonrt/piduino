79f9d0d2ddc804aee6ac529fbb1d03ae9bf407f6
epsilonrt <epsilonrt@gmail.com>
2018-06-05 16:49:10 +0000

    Cleaning examples directory

--- examples/noarduino/gpio/fade/fade.cpp
+++ examples/noarduino/gpio/fade/fade.cpp
@@ -1,19 +1,12 @@
 /*
-  Fading
+  Fade
 
-  This example shows how to fade an LED using the analogWrite() function.
-
-  The circuit:
-  - LED attached from digital pin 9 to ground.
-
-  created 1 Nov 2008
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe
+  This example shows how to fade an LED on pin 0 using the analogWrite()
+  function.
 
   This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/Fading
+  http://www.arduino.cc/en/Tutorial/Fade
 */
 #include <iostream>
 #include <piduino/clock.h>
