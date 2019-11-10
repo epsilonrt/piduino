680628fdcd28a44d53ad5d438946ae126a799ee0
epsilonrt <epsilonrt@gmail.com>
2019-11-10 17:37:30 +0100

    Update copyright year

--- utils/pido/main.cpp
+++ utils/pido/main.cpp
@@ -691,7 +691,7 @@ void
 version() {
 
   cout << "Piduino pido version " << VERSION_SHORT << endl;
-  cout << "Copyright © 2018 " << authors << ", " << website << endl;
+  cout << "Copyright © 2018-2019 " << authors << ", " << website << endl;
   cout << "This program comes with ABSOLUTELY NO WARRANTY." << endl;
   cout << "This is free software, and you are welcome to redistribute it" << endl;
   cout << "under certain conditions; type 'pido -w' for details." << endl << endl;
@@ -701,14 +701,14 @@ version() {
 void
 warranty () {
   cout <<
-       "Copyright © 2018 " << authors << ", All rights reserved.\n\n"
+       "Copyright © 2018-2019 " << authors << ", All rights reserved.\n\n"
 
        " Piduino pido is free software: you can redistribute it and/or modify\n"
        " it under the terms of the GNU General Public License as published by\n"
        " the Free Software Foundation, either version 3 of the License, or\n"
        " (at your option) any later version.\n\n"
 
-       "  Piduino pido is distributed in the hope that it will be useful,\n"
+       " Piduino pido is distributed in the hope that it will be useful,\n"
        " but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
        " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
        " GNU General Public License for more details.\n\n"
